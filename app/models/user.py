1bf0f2f4d427cac7de3191db257cc918adf986df
dtitus929 <dave@creativegozone.com>
2023-04-10 00:13:24 -0400

    User Routes Ready for Review

--- app/models/user.py
+++ app/models/user.py
@@ -10,12 +10,12 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
-    avatar = db.Column(db.String, default='https://ca.slack-edge.com/T0266FRGM-UQ46QH94Z-gc24d346e359-512')
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    avatar = db.Column(db.String(255), default='https://ca.slack-edge.com/T0266FRGM-UQ46QH94Z-gc24d346e359-512')
     bio = db.Column(db.String(2000))
 
     @property
@@ -33,5 +33,9 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self. last_name,
+            'avatar': self.avatar,
+            'bio': self.bio
         }

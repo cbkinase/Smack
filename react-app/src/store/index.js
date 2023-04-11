1e70957877d696cd0dafc1547f34522eeaeb02a0
brianhitchin <brianleehitchin@gmail.com>
2023-04-11 16:32:40 -0700

    AAAAAAA

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session'
+import reducer from './session'
 import channelReducer from './channel';
 
 const rootReducer = combineReducers({
-  session,
+  session: reducer,
   channel: channelReducer
 });
 

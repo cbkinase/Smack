52135ba82e4542f4cc602b72ceabebb682355ff1
cbkinase <cambeck366@gmail.com>
2023-04-12 14:56:21 -0400

    sockets working??

--- react-app/src/store/channel.js
+++ react-app/src/store/channel.js
@@ -1,123 +1,123 @@
-const initialState = {}
+const initialState = {};
 
-const ALL_CHANNEL = 'channel/all'
-const USER_CHANNELS = 'channel/user'
-const GET_ONE_CHANNEL = 'channel/getone'
-const ADD_CHANNEL = 'channel/add'
-const EDIT_CHANNEL = 'channel/edit'
-const DELETE_CHANNEL = 'channel/delete'
+const ALL_CHANNEL = "channel/all";
+const USER_CHANNELS = "channel/user";
+const GET_ONE_CHANNEL = "channel/getone";
+const ADD_CHANNEL = "channel/add";
+const EDIT_CHANNEL = "channel/edit";
+const DELETE_CHANNEL = "channel/delete";
 
 export const UserChannel = (payload) => {
     return {
         type: USER_CHANNELS,
-        payload
-    }
-}
+        payload,
+    };
+};
 
 export const OneChannel = (payload) => {
     return {
         type: GET_ONE_CHANNEL,
-        payload
-    }
-}
+        payload,
+    };
+};
 
 export const AddChannel = (payload) => {
     return {
         type: ADD_CHANNEL,
-        payload
-    }
-}
+        payload,
+    };
+};
 
 export const EditChannel = (payload) => {
     return {
         type: EDIT_CHANNEL,
-        payload
-    }
-}
+        payload,
+    };
+};
 
 export const AllChannel = (payload) => {
     return {
         type: ALL_CHANNEL,
-        payload
-    }
-}
+        payload,
+    };
+};
 
 export const DeleteChannel = (id) => {
     return {
         type: DELETE_CHANNEL,
-        id
-    }
-}
+        id,
+    };
+};
 
-export const AllChannelThunk = () => async dispatch => {
-    const response = await csrfFetch(`/api/channels/all`)
+export const AllChannelThunk = () => async (dispatch) => {
+    const response = await csrfFetch(`/api/channels/all`);
 
     if (response.ok) {
         const data = await response.json();
         dispatch(AllChannel(data));
     }
-}
+};
 
-export const OneChannelThunk = (id) => async dispatch => {
-    const response = await csrfFetch(`/api/channels/${id}`)
+export const OneChannelThunk = (id) => async (dispatch) => {
+    const response = await csrfFetch(`/api/channels/${id}`);
 
     if (response.ok) {
         const data = await response.json();
         dispatch(OneChannel(data));
     }
-}
+};
 
-export const UserChannelThunk = () => async dispatch => {
-    const response = await csrfFetch(`/api/channels/user`)
+export const UserChannelThunk = () => async (dispatch) => {
+    const response = await csrfFetch(`/api/channels/user`);
 
     if (response.ok) {
         const data = await response.json();
         dispatch(UserChannel(data));
     }
-}
+};
 
-export const EditChannelThunk = (id, body) => async dispatch => {
+export const EditChannelThunk = (id, body) => async (dispatch) => {
     const response = await fetch(`/api/channels/${id}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(body)
-    })
+        body: JSON.stringify(body),
+    });
 
     if (response.ok) {
         const data = await response.json();
-        dispatch(EditChannel(data))
+        dispatch(EditChannel(data));
     }
-}
+};
 
-export const DeleteChannelThunk = (id) => async dispatch => {
+export const DeleteChannelThunk = (id) => async (dispatch) => {
     const response = await csrfFetch(`/api/channels/${id}`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-
-    if (response.ok) {
-        dispatch(DeleteChannel(id))
-    }
-}
-
-export const AddChannelThunk = (value) => async dispatch => {
-    const response = await csrfFetch('/api/groups', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(value)
-    })
+    });
 
     if (response.ok) {
-        const data = await response.json()
-        dispatch(AddChannel(data))
+        dispatch(DeleteChannel(id));
     }
-}
+};
+
+export const AddChannelThunk = (value) => async (dispatch) => {
+    const response = await csrfFetch("/api/groups", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(value),
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(AddChannel(data));
+    }
+};
 
 const channelReducer = (state = initialState, action) => {
     let newState;
@@ -136,31 +136,48 @@ const channelReducer = (state = initialState, action) => {
             return newState;
         case DELETE_CHANNEL:
             newState = Object.assign({}, state);
-            delete newState.channels.all_channels[action.id]
-            if (Object.keys(newState.channels.user_channels).includes(action.payload.id)) {
-                delete newState.channels.user_channels[action.payload.id]
+            delete newState.channels.all_channels[action.id];
+            if (
+                Object.keys(newState.channels.user_channels).includes(
+                    action.payload.id
+                )
+            ) {
+                delete newState.channels.user_channels[action.payload.id];
             }
-            if (Object.keys(newState.channels.single_channel).includes(action.payload.id)) {
-                delete newState.channels.single_channel[action.payload.id]
+            if (
+                Object.keys(newState.channels.single_channel).includes(
+                    action.payload.id
+                )
+            ) {
+                delete newState.channels.single_channel[action.payload.id];
             }
-            return newState
+            return newState;
         case EDIT_CHANNEL:
-            newState = Object.assign({}, state)
-            newState.channels.all_channels[action.payload.id] = action.payload
-            if (Object.keys(newState.channels.user_channels).includes(action.payload.id)) {
-                newState.channels.user_channels[action.payload.id] = action.payload
+            newState = Object.assign({}, state);
+            newState.channels.all_channels[action.payload.id] = action.payload;
+            if (
+                Object.keys(newState.channels.user_channels).includes(
+                    action.payload.id
+                )
+            ) {
+                newState.channels.user_channels[action.payload.id] =
+                    action.payload;
             }
-            if (Object.keys(newState.channels.single_channel).includes(action.payload.id)) {
-                newState.channels.single_channel = action.payload
+            if (
+                Object.keys(newState.channels.single_channel).includes(
+                    action.payload.id
+                )
+            ) {
+                newState.channels.single_channel = action.payload;
             }
-            return newState
+            return newState;
         case ADD_CHANNEL:
-            newState = Object.assign({}, state)
-            newState.channels.all_channels[action.payload.id] = action.payload
-            newState.channels.user_channels[action.payload.id] = action.payload
-            newState.channels.single_channel = action.payload
+            newState = Object.assign({}, state);
+            newState.channels.all_channels[action.payload.id] = action.payload;
+            newState.channels.user_channels[action.payload.id] = action.payload;
+            newState.channels.single_channel = action.payload;
             return newState;
     }
-}
+};
 
-export default channelReducer
+export default channelReducer;

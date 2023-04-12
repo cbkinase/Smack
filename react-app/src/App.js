cc33eb55f34d04f945bb68ae48cabc083396e252
dtitus929 <dave@creativegozone.com>
2023-04-12 18:05:37 -0400

    Added LoginSignUp Landing Page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,40 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
-import Navigation from "./components/Navigation";
-import Chat from "./components/ChatTest";
-
-import ReactionTestPage from "./components/ReactionTest";
+import { useSelector } from 'react-redux';
+import LoginSignupPage from "./components/LoginSignupPage";
+import ProfileButton from './components/Navigation/ProfileButton';
+// import Chat from "./components/ChatTest";
+// import ReactionTestPage from "./components/ReactionTest";
 
 function App() {
+
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(authenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  const sessionUser = useSelector(state => state.session.user);
+
   return (
     <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-          <Route path="/reactions">
-            <ReactionTestPage />
-          </Route>
-          <Route path="/chat_test/:channelId">
-            <Chat />
-          </Route>
-        </Switch>
+
+      {sessionUser ? (
+        <ProfileButton user={sessionUser} />
+      ) : (
+        <LoginSignupPage />
       )}
+
     </>
   );
 }

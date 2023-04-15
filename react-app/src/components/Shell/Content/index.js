6bfa4b866fea46071685a6019627263ab3216c8f
dtitus929 <dave@creativegozone.com>
2023-04-15 10:45:14 -0400

    Grid Editor Removed

--- react-app/src/components/Shell/Content/index.js
+++ react-app/src/components/Shell/Content/index.js
@@ -6,7 +6,7 @@ import OneChannel from "../../OneChannel";
 import CreateChannel from "../Content/Channels/ChannelCreator";
 import EditChannel2 from "../Content/Channels/ChannelEditor";
 
-function Content({selectedUserRightBar, setSelectedUserRightBar}) {
+function Content({ selectedUserRightBar, setSelectedUserRightBar }) {
     return (
         <>
             <div
@@ -16,13 +16,13 @@ function Content({selectedUserRightBar, setSelectedUserRightBar}) {
                 <ChannelHeader />
             </div>
 
-            <EditChannel2 />
+            {/* <EditChannel2 /> */}
             <div id="grid-content" className="grid-content-threecolumn">
-                <Messages selectedUserRightBar={selectedUserRightBar} setSelectedUserRightBar={setSelectedUserRightBar}  />
+                <Messages selectedUserRightBar={selectedUserRightBar} setSelectedUserRightBar={setSelectedUserRightBar} />
             </div>
-            <div id="grid-editor" className="grid-editor-threecolumn">
-                {/* <Editor /> */}
-            </div>
+            {/* <div id="grid-editor" className="grid-editor-threecolumn"> */}
+            {/* <Editor /> */}
+            {/* </div> */}
         </>
     );
 }

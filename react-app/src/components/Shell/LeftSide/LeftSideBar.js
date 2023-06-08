4d4c3c952cfddc8fe16c96ebb98bee6b09919e19
cbkinase <cambeck366@gmail.com>
2023-06-08 15:46:15 -0400

    about me section added

--- react-app/src/components/Shell/LeftSide/LeftSideBar.js
+++ react-app/src/components/Shell/LeftSide/LeftSideBar.js
@@ -143,6 +143,55 @@ function LeftSideLinks() {
 
 
             </div>
+            <div style={{ position: 'absolute', bottom: '0px' }}>
+                <div className="footer" style={{ padding: '20px', justifyContent: 'flex-start', alignItems: "flex-start", backgroundColor: '#3f0e40' }}>
+
+                    <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: '5px', textAlign: 'left' }}>
+
+                        <div>
+                            <span style={{ color: '#969696' }}>Cameron Beck:</span>
+                        </div>
+
+                        <div className="footer-link">
+                            <span>
+                                <a className="footer-button" href="https://cbkinase.github.io/" target="_blank" rel="noreferrer">
+                                    <button className="copyright-button2">
+                                        <i className="fas fa-eye" style={{ fontSize: '14px' }}></i>
+                                    </button>
+                                </a>
+                            </span>
+                        </div>
+
+                        <div className="footer-link">
+                            <span>
+                                <a className="footer-button" href="https://github.com/cbkinase/Smack" target="_blank" rel="noreferrer">
+                                    <button className="copyright-button2">
+                                        <i className="fa fa-github" style={{ fontSize: '14px' }}></i>
+                                    </button>
+                                </a>
+                            </span>
+                        </div>
+
+                        <div className="footer-link">
+                            <span>
+                                <a className="footer-button" href="https://www.linkedin.com/in/cameron-beck-4a9a44274/" target="_blank" rel="noreferrer">
+                                    <button className="copyright-button2">
+                                        <i className="fa fa-linkedin-square" style={{ fontSize: '14px' }}></i>
+                                    </button>
+
+
+                                </a>
+                            </span>
+                        </div>
+
+                    </div >
+
+                    <div className="footer-link" style={{ fontSize: '11px' }}>Additional Contributors:<br />Dave Titus,&nbsp;&nbsp;Brian Hitchin,&nbsp;&nbsp;Cynthia Liang</div>
+
+
+
+                </div >
+            </div>
 
 
         </div>

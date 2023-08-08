6ff9a83c8d0bdfca07d5f03bb9ede3658e06bbfa
cbkinase <cambeck366@gmail.com>
2023-08-07 22:42:11 -0400

    Deleting attachments on seed undo

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,6 @@
 from flask.cli import AppGroup
 from app.models.db import environment, db
-from app.models import channel_user
+from app.models import channel_user, Attachment
 from .seed_users import seed_users, undo_users
 from .seed_channels import seed_channels, undo_channels
 from .seed_messages import seed_messages, undo_messages
@@ -18,6 +18,16 @@ def undo_channel_users():
         print(f"Error occurred while deleting entries from channel_users: {e}")
 
 
+def undo_attachments():
+    try:
+        num_deleted = db.session.query(Attachment).delete()
+        db.session.commit()
+        print(f"Deleted {num_deleted} entries from Attachment")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error occurred while deleting entries from Attachment: {e}")
+
+
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
@@ -30,6 +40,7 @@ def seed():
         # Before seeding in production, you want to run the seed undo
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in seed_users.py undo_users function).
+        undo_attachments()
         undo_reactions()
         undo_messages()
         undo_channel_users()
@@ -47,6 +58,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_attachments()
     undo_reactions()
     undo_messages()
     undo_channel_users()

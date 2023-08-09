1d20976b03516cf6a03db4682d376ce288e315e7
cynthialiang00 <cynliang00@gmail.com>
2023-08-08 20:20:44 -0700

    refactor_2.5 removed useless import in messageUser.js

--- react-app/src/components/Shell/Content/Messages/MessageCard/MessageParts/MessageUser.js
+++ react-app/src/components/Shell/Content/Messages/MessageCard/MessageParts/MessageUser.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React from "react";
 import toggleOnRightPane from "../../../../RightSide/toggleOnRightPane";
 
 export default function MessageUser({messageUser, user, setSelectedUserRightBar}) {

ea8253dcd95d667a7bdc176bfcbf47284dae6839
cbkinase <cambeck366@gmail.com>
2023-09-03 03:26:40 -0400

    Additional refactors to emoji modal

--- react-app/src/components/EmojiModal/index.js
+++ react-app/src/components/EmojiModal/index.js
@@ -1,24 +1,31 @@
 import { emojis } from "../../utils/Emoji";
+import "./EmojiStylesheet.css";
 
 export default function EmojiModal({ onClickFunction }) {
 
+    function renderEmojiSection(emojiSectionTitle) {
+        const emojiList = emojis[emojiSectionTitle];
+        return (
+            <section className="emoji-modal-section" key={emojiSectionTitle}>
+                <h3 className="emoji-section-title">
+                    {emojiSectionTitle}
+                </h3>
+                <div className="emoji-section-content">
+                    {emojiList.map(renderButton)}
+                </div>
+            </section>
+        )
+    }
+
+    function renderButton(emoji) {
+        return <button key={emoji} className="reaction-icon" onClick={onClickFunction}> {emoji} </button>
+    }
+
+    if (!emojis) return null;
+
     return (
-        <>
-            <div
-                style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    maxWidth: "320px",
-                    justifyContent: "center",
-                    gap: '2px'
-                }}
-            >
-                {emojis.map((emoji) => (
-                    <button key={emoji} className="reaction-icon" onClick={onClickFunction} >
-                        {emoji}
-                    </button>
-                ))}
+            <div id="emoji-modal-container">
+                {Object.keys(emojis).map(renderEmojiSection)}
             </div>
-        </>
     );
 }

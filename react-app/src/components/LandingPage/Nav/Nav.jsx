0dd73d5105191e52b18b8ae70e79e51b223656db
cbkinase <cambeck366@gmail.com>
2023-09-03 19:49:10 -0400

    Login now Navlink

--- react-app/src/components/LandingPage/Nav/Nav.jsx
+++ react-app/src/components/LandingPage/Nav/Nav.jsx
@@ -1,12 +1,13 @@
 import smackLogo from '../../Shell/LeftSide/smack-logo-white.svg';
 import './Nav.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Nav () {
     return (
         <nav className="landing-nav">
             <div className="landing-nav-content">
                     <img src={smackLogo} alt="smack logo"></img>
-                    <a href='/login'>Log In</a>
+                    <NavLink to='/login'>Log In</NavLink>
 
             </div>
         </nav>

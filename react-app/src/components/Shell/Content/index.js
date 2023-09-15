a3769396b34ab6fd611c533ac0af5cc38bb6e0a6
cbkinase <cambeck366@gmail.com>
2023-09-15 18:44:32 -0400

    Eh...

--- react-app/src/components/Shell/Content/index.js
+++ react-app/src/components/Shell/Content/index.js
@@ -19,7 +19,7 @@ function Content() {
                 ref={scrollContainerRef}
                 id="grid-content"
                 className="grid-content-threecolumn"
-                style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginBottom: '116px' }}
+                style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}
             >
                 <Messages scrollContainerRef={scrollContainerRef} />
             </div>
